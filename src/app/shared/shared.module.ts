7d94b8649b27dadc587397096cceeb23db50aa31
greg-doneup <greg.brownnyc@gmail.com>
2023-01-02 15:37:11 -0500

    landing page complete (needs link routes to be added to buttons)

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,26 @@
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+    declarations: [
+
+    ],
+    imports: [
+        CommonModule,
+        IonicModule,
+        FormsModule,
+        ReactiveFormsModule
+    ],
+    exports: [
+
+    ],
+    entryComponents: [
+      
+    ],
+    schemas: [NO_ERRORS_SCHEMA]
+})
+export class SharedModule { }
